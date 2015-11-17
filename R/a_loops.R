2e529da7b4381a7883f0394c88944730fc33fee7
bohenderson <bohenderson93@gmail.com>
2015-11-17 17:04:18 -0500

    python file still needs a lot of work

--- R/a_loops.R
+++ R/a_loops.R
@@ -1,23 +1,17 @@
 library(RMySQL)
-
 ### connect to local db
 mydb = dbConnect(MySQL(), user='rojahend', password='password', dbname='buses',
     host='localhost')
 dbListTables(mydb)
-
 dbGetQuery(mydb, 'describe intervaldata;')
-
 ### get all the data and then get a small chunk of it.
 intervaldata <- dbGetQuery(mydb, 'SELECT * FROM intervaldata;')
 intdat <- dbGetQuery(mydb, 'SELECT * FROM intervaldata limit 1000;')
-
 ### all routeids and their frequency. We will start by focusing on route 331
 ARoute <- dbGetQuery(mydb, 'SELECT * FROM intervaldata where routeid=331;')
-
 ### only the stops that have been visited more than ~1000 times really matter
 trueStops <- as.numeric(names(table(ARoute$to)[which(table(ARoute$to) > 1000)]))
 trueStopstr <- paste(as.character(trueStops), collapse=",")
-
 ### modify ARoute to only include datapoints for trueStops
 query <- paste("SELECT * FROM intervaldata where routeid=331 and `to` IN (", trueStopstr, ") and `from` IN (", trueStopstr, ") order by `when`;", collapse="")
 ARoute <- dbGetQuery(mydb, query)
@@ -26,9 +20,12 @@ stop <- 35
 routeProgression = list()
 for (i in 1:length(trueStops)) {
     ## get the two most common `to` values associated with each `from`
-    posibilities <- names(sort(
+    posibilities <- sort(
         table(ARoute$to[which(ARoute$from == stop)]),
-        decreasing=TRUE)[1:2])
+        decreasing=TRUE)
+    print(stop)
+    print(posibilities)
+    posibilities <- names(posibilities)
     ## one of these `to` values will match `from`. Get the other one.
     if (as.numeric(posibilities[1]) == stop){
         stop <- posibilities[2] }
@@ -39,3 +36,6 @@ for (i in 1:length(trueStops)) {
 
 unlist(routeProgression)
 ### 67->39->38->37->41->1->4->6->8->10->11->12->13->14->36->30->34->35
+
+### show all `from` datapoints with `to`=67 and their frequencies
+sort(table(ARoute$from[which(ARoute$to == 67)]),decreasing=TRUE)
--- py/a_loops.py
+++ py/a_loops.py
@@ -14,22 +14,66 @@ cursor = mysqldb.cursor()
 # these stops and their order were calculated using a_loops.R
 aStops = [67, 39, 38, 37, 41, 1, 4, 6, 8, 10, 11, 12, 13, 14, 36, 30, 34, 35]
 
+# USED TO BUILD THE FOLLOWING QUERY
 # construct the MySQL query
-q = ""
-old = aStops[0]
-for i in aStops:
-    q = q + """
-    (`from`=%(fr)d and `to`=%(to)d) or """ % {"fr": old, "to": i}
-    old = i
-    q = q + """
-    (`from`=%(fr)d and `to`=%(to)d) or """ % {"fr": old, "to": i}
+# q = ""
+# old = aStops[0]
+# for i in aStops:
+#     q = q + """
+#     (`from`=%(fr)d and `to`=%(to)d) or """ % {"fr": old, "to": i}
+#     old = i
+#     q = q + """
+#     (`from`=%(fr)d and `to`=%(to)d) or """ % {"fr": old, "to": i}
+# query = """
+# SELECT * FROM intervaldata where
+# route_id=331 and (""" + q[:-4] + """)
+# order by bus_id asc, `when` asc;
+# """
+# query = query.replace('67', '35', 1)
+# print query
+
 query = """
 SELECT * FROM intervaldata where
-route_id=331 and (""" + q[:-4] + """)
+route_id=331 and (
+(`from`=67 and `to`=35) or
+(`from`=67 and `to`=11) or
+(`from`=67 and `to`=67) or
+(`from`=67 and `to`=39) or
+(`from`=39 and `to`=39) or
+(`from`=39 and `to`=38) or
+(`from`=38 and `to`=38) or
+(`from`=38 and `to`=37) or
+(`from`=37 and `to`=37) or
+(`from`=37 and `to`=41) or
+(`from`=41 and `to`=41) or
+(`from`=41 and `to`=1) or
+(`from`=1 and `to`=1) or
+(`from`=1 and `to`=4) or
+(`from`=4 and `to`=4) or
+(`from`=4 and `to`=6) or
+(`from`=6 and `to`=6) or
+(`from`=6 and `to`=8) or
+(`from`=8 and `to`=8) or
+(`from`=8 and `to`=10) or
+(`from`=10 and `to`=10) or
+(`from`=10 and `to`=11) or
+(`from`=11 and `to`=11) or
+(`from`=11 and `to`=12) or
+(`from`=12 and `to`=12) or
+(`from`=12 and `to`=13) or
+(`from`=13 and `to`=13) or
+(`from`=13 and `to`=14) or
+(`from`=14 and `to`=14) or
+(`from`=14 and `to`=36) or
+(`from`=36 and `to`=36) or
+(`from`=36 and `to`=30) or
+(`from`=30 and `to`=30) or
+(`from`=30 and `to`=34) or
+(`from`=34 and `to`=34) or
+(`from`=34 and `to`=35) or
+(`from`=35 and `to`=35))
 order by bus_id asc, `when` asc;
 """
-query = query.replace('67', '35', 1)
-# print query
 
 # execute the query for selecting valid a-route intervals
 try:
@@ -38,13 +82,14 @@ except:
     print "error"
     sys.exit()
 
-fout = open("A_Route.csv", 'w')
+# output = the header line of my output csv: 38 column names
+output = "day,busid"
+for n in aStops:
+    output = output + ",to" + str(n) + ",from" + str(n)
+print output
 
-# output = the header line of my output csv, 38 columns
-output = "day,busid,67"
-for n in aStops[1:]:
-    output = output + ",to" + str(n) + "," + str(n)
-output = output + ",to67"
+# write the column names to an output file
+fout = open("A_Route.csv", 'w')
 fout.write(output)
 
 # row format:
@@ -54,14 +99,16 @@ output = ""
 inLoop = False
 for i in range(1, cursor.rowcount):
     line = cursor.fetchone()
-    if line[1] == 67 and line[2] == 67:
+    if line[2] == 67:
         inLoop = True
+        # when we start a loop, start by printing the date and busid
         output = str(line[6].year) + "-" + str(line[6].month)\
             + "-" + str(line[6].day) + "," + str(line[4]) + ","
     if inLoop:
+        # if we're in a loop, add the timestamp of each stop
         output = output + str(line[6].hour) + ":" + str(line[6].minute)\
             + ":" + str(line[6].second) + ","
-    if line[1] == 35 and line[2] == 67:
+    if line[1] == 35 and line[2] == 35:
         fout.write(output[:-1] + "\n")
         inLoop = False
 
