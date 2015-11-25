7007a9338d69532af831e0a1f510a666a9a94f8b
bohenderson <bohenderson93@gmail.com>
2015-11-25 18:41:24 -0500

    rewrote a_loops.py and it should be ready to go

--- py/a_loops.py
+++ py/a_loops.py
@@ -4,6 +4,9 @@ import MySQLdb
 # from datetime import date
 # from datetime import timedelta
 
+# add 'limit 1000' to last row of query if DEBUG = True
+DEBUG = False
+
 
 def maybeAddZero(intput):
     if intput < 10:
@@ -18,8 +21,7 @@ def getDate(datetime):
 
 
 def getTime(datetime):
-    timeout = ""
-    timeout = timeout + maybeAddZero(datetime.hour)
+    timeout = maybeAddZero(datetime.hour)
     timeout = timeout + ":"
     timeout = timeout + maybeAddZero(datetime.minute)
     timeout = timeout + ":"
@@ -27,15 +29,50 @@ def getTime(datetime):
     return timeout
 
 
-# I pulled this chunk out to prevent duplication
-# j and k are indices of aStops and aStops[j],aStops[k] returns the values:
-#    67,67 -> increment -> 67,39 -> increment -> 39,39 -> etc
+# writes out the current row and sets up a new one
+def newRow(line):
+    when_output = getDate(line[6]) + ',' + str(line[4]) + ','
+    time_output = getDate(line[6]) + ',' + str(line[4]) + ','
+    return [when_output, time_output]
+
+
+def writeRow(when_output, time_output):
+    global DEBUG
+    # fill any missing columns with blanks
+    while when_output.count(',') < 37:
+        when_output = when_output + ','
+        time_output = time_output + ','
+    if DEBUG:
+        print when_output, '\n'
+    global fout_when
+    fout_when.write(when_output[:-1] + "\n")
+    if DEBUG:
+        print time_output, '\n\n'
+    global fout_time
+    fout_time.write(time_output[:-1] + "\n")
+
+
+# j and k are `to` and `from` indices of aStops respectively
+# aStops[j],aStops[k] returns the values:
+# 67,67 -> increment -> 67,39 -> increment -> 39,39 -> etc
+# and these indicies will loop through the stops repeatedly
 def increment(j, k):
     if j == k:
-        j = j + 1
+        # if we're at the last stop, return to the first one
+        if j == 17:
+            j = 0
+        else:
+            j = j + 1
     else:
         k = j
     return [j, k]
+
+
+def resetJK():
+    global DEBUG
+    if DEBUG:
+        print "resetting incrementors..."
+    return [0, 17]
 
 
 # connect to remote mysql server
@@ -68,7 +105,6 @@ aStops = [67, 39, 38, 37, 41, 1, 4, 6, 8, 10, 11, 12, 13, 14, 36, 30, 34, 35]
 query = """
 SELECT * FROM intervaldata where
 route_id=331 and (
-(`from`=11 and `to`=67) or
 (`from`=35 and `to`=67) or
 (`from`=67 and `to`=67) or
 (`from`=67 and `to`=39) or
@@ -106,7 +142,7 @@ route_id=331 and (
 (`from`=34 and `to`=35) or
 (`from`=35 and `to`=35))
 order by bus_id asc, `when` asc
-limit 1000
+;
 """
 
 # execute the query for selecting valid a-route intervals
@@ -120,22 +156,26 @@ except:
 output = "day,busid"
 for n in aStops:
     output = output + ",to" + str(n) + ",from" + str(n)
-print output
+if DEBUG:
+        print output
 
 # write the column names to an output file
 fout_when = open("a_route_when.csv", 'w')
 fout_time = open("a_route_time.csv", 'w')
-fout_when.write(output)
-fout_time.write(output)
+fout_when.write(output + "\n")
+fout_time.write(output + "\n")
+
+# `from` frequency across all a-route bus stops with `to` set as 67:
+#   67   35   11   34   41   10   30   39   36   13    1    8   14   38
+# 6852 6473  237   48   24   20    6    5    3    2    1    1    1    1
 
 # query row format:
 # (id, from, to, time, busid, routeid, datetime)
 # example query row:
 # (12345, 8L, 10L, 42L, 641L, 331L, datetime.datetime(2014, 8, 19, 11, 20, 25))
 
-# `from` frequency across all a-route bus stops with `to` set as 67:
-#   67   35   11   34   41   10   30   39   36   13    1    8   14   38
-# 6852 6473  237   48   24   20    6    5    3    2    1    1    1    1
+# first column: to67,  line[1] == 35, line[2] == 67  aka  j = 17, k = 0
+# last column: from35, line[1] == 35, line[2] == 35  aka  j = 17, k = 17
 
 # clear the column names from output and initialize loop variables
 when_output = ""
@@ -143,88 +183,68 @@ time_output = ""
 inLoop = False
 shouldWrite = True
 
-# j and k are indices of aStops
-# aStops[j],aStops[k] returns the values:
-# 67,67 -> increment -> 67,39 -> increment -> 39,39 -> etc
-# k = to
-k = -1
-# j = from
-j = 0
+# k = to, j = from
+[j, k] = resetJK()
 
 # for every row in the result of our mysql query...
 for i in range(1, cursor.rowcount):
     line = cursor.fetchone()
-    shouldWrite = True
 
-    # if we see the start of a loop...
-    if line[2] == 67 and line[1] != 67:
-        # and if we're currently in a loop
-        if inLoop:
-            # fill in the rest of the current `when` entry with blanks
-            while when_output.count(',') < 37:
-                when_output = when_output + ','
-            # write out the current `when` entry
-            print "`when` ended early.."
-            print when_output[:-1], "\n"
-            fout_when.write(when_output[:-1] + "\n")
-            # fill in the rest of the current `time` entry with blanks
-            while time_output.count(',') < 37:
-                time_output = time_output + ','
-            # write out the current `time` entry
-            print "`time` ended early.."
-            print time_output[:-1], "\n"
-            fout_time.write(time_output[:-1] + "\n")
-        inLoop = True
-        # when we start a loop, start by printing the date and busid to both
-        when_output = getDate(line[6]) + "," + str(line[4]) + ","
-        time_output = getDate(line[6]) + "," + str(line[4]) + ","
+    # if we're just starting out, start a new row
+    if when_output == "" or time_output == "":
+        if DEBUG:
+            print "let's start!"
+        [when_output, time_output] = newRow(line)
+        [j, k] = resetJK()
+    # else, if we're suddenly looking at a different day
+    elif getDate(line[6]) != when_output[0:10]:
+        # let us know and start a new row
+        if DEBUG:
+            print "current day ", when_output[0:10],
+            print " doesn't match ", getDate(line[6]), "...terminate!"
+        writeRow(when_output, time_output)
+        [when_output, time_output] = newRow(line)
+        [j, k] = resetJK()
+    # else, if we're suddenly looking at a different bus
+    elif str(line[4]) != when_output[11:14]:
+        # let us know and start a new row
+        if DEBUG:
+            print "current bus ", str(line[4])
+            print " doesn't match ", when_output[11:14], "...terminate!"
+        writeRow(when_output, time_output)
+        [when_output, time_output] = newRow(line)
+        [j, k] = resetJK()
 
-    # If we're in a loop...
-    if inLoop:
-        # if this row doesn't match the right column of the output file...
-        if line[1] != aStops[k] or line[2] != aStops[j]:
-            print "Mismatch!"
-            # assume the bus skipped this stop, check the next until
-            #   either a match is found or we run out of stops
-            while True:
-                print "skipped stop"
-                print "MISMATCH! expected from aStops[" + str(k) + "]="\
-                    + str(aStops[k]), " to aStops[" + str(j) + "]="\
-                    + str(aStops[j])
-                print "observed:",
-                print line[1:]
-                when_output = when_output + ","
-                time_output = time_output + ","
-                [j, k] = increment(j, k)
-                # check to see whether we match yet
-                if line[1] == aStops[k] and line[2] == aStops[j]:
-                    print "found a match! onwards!!"
-                    break
-                if j + k == 34:
-                    print "ran out of stops"
-                    shouldWrite = False
-                    break
-        if shouldWrite:
-            when_output = when_output + getTime(line[6]) + ","
-            time_output = time_output + str(line[3]) + ","
-
-        # if this row matches the last column of our when_output table...
-        if (line[1] == 35 and line[2] == 35) or j + k == 34:
-            fout_when.write(when_output[:-1] + "\n")
-            fout_time.write(time_output[:-1] + "\n")
-            # print what we're writing out
-            print when_output[:-1], "\n"
-            print time_output[:-1], "\n\n"
-            # reset our iterators
-            j = -1
-            k = -1
-            # we're done with this loop
-            inLoop = False
-
+    # if this row doesn't match the current column...
+    while line[1] != aStops[k] or line[2] != aStops[j]:
+        if DEBUG:
+            print "trying to match: ", line[1], line[2],
+            print "\tcurrently looking at ", aStops[k], aStops[j]
+        # move forward until we find a match
+        when_output = when_output + ","
+        time_output = time_output + ","
+        # make a new row if it comes to that...
+        if j == 17 and k == 17:
+            if DEBUG:
+                print "looks like we'll have to continue on the next row..."
+            writeRow(when_output, time_output)
+            [when_output, time_output] = newRow(line)
+            [j, k] = resetJK()
         [j, k] = increment(j, k)
 
-    else:
-        print "skipped line:", line[1:]
+    # after escaping the above while loop, this line should match this column
+    # add data to this column and move on to the next
+    when_output = when_output + getTime(line[6]) + ","
+    time_output = time_output + str(line[3]) + ","
+    [j, k] = increment(j, k)
+
+    # if we hit the last column (from35), set up a new row
+    if line[1] == 35 and line[2] == 35:
+        if DEBUG:
+            print "we've reached the end of the line!"
+        writeRow(when_output, time_output)
+        [when_output, time_output] = newRow(line)
+        [j, k] = resetJK()
 
 fout_when.close
 fout_time.close
